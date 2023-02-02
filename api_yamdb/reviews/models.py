68c81cd6c8d1677f17971ab12e7091126b980166
Petr Oganin <necroshizo@gmail.com>
2023-02-02 21:40:57 +0300

    +model review and comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator 
+from django.core.validators import MaxValueValidator, MinValueValidator
+from users.models import User
 from datetime import date
 
 
@@ -80,3 +81,67 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    """Модель отзовов на произведение"""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор',
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение',
+    )
+    score = models.IntegerField('Оценка', validators=[
+        MaxValueValidator(limit_value=10, message='Оценка не более 10'),
+        MinValueValidator(limit_value=1, message='Оценка не меньше 1'),
+    ])
+    text = models.TextField('Отзов')
+    pud_data = models.DateTimeField(
+        'Дата публикации', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def __str__(self):
+        return (
+            f'Пользователь {self.author} '
+            f'оценил на {self.score} '
+            f'произведение "{self.title.name}".'
+        )
+
+
+class Comment(models.Model):
+    """Модель коментариев к отзывам"""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор',
+    )
+
+    reviews = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв',
+    )
+    text = models.TextField('Коментарий')
+    pud_data = models.DateTimeField(
+        'Дата публикации', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Коментарий'
+        verbose_name_plural = 'Коментарии'
+
+    def __str__(self):
+        return (
+            f'Пользователь {self.author} оставил коментарий'
+            f'к отзыву {self.reviews.author.username}'
+        )
