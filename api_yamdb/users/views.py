71a97759aa90bbf10ec03065256ca599ced6ffc0
Денис Филиппов <denbfil@yandex.ru>
2023-02-03 16:05:58 +0300

    Работа с пользователями после исправления ошибок Pytest

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,25 +1,28 @@
+from api.permissions import IsAdmin
 from django.contrib.auth.tokens import default_token_generator
 from django.core import mail
 from django.shortcuts import get_object_or_404
-from rest_framework import status, viewsets, permissions
-from rest_framework.decorators import action, api_view
+from rest_framework import filters, permissions, status, viewsets
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.validators import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
-
-from api_yamdb.settings import GENERAL_EMAIL
 from users.models import User
 from users.serializers import (GetTokenSerializer, SignupSerializer,
-                               UserSerializer, UserEditSerializer)
-from api.permissions import IsAdmin
-from rest_framework.permissions import IsAuthenticated
+                               UserEditSerializer, UserSerializer)
+
+from api_yamdb.settings import GENERAL_EMAIL
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
     lookup_field = 'username'
     permission_classes = [IsAuthenticated, IsAdmin, ]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
 
     @action(
         methods=['GET', 'PATCH'],
@@ -27,7 +30,7 @@ class UserViewSet(viewsets.ModelViewSet):
         permission_classes=[permissions.IsAuthenticated],
     )
     def me(self, request):
-        user = User.objects.get(username=request.user.username)
+        user = request.user
         if request.method == 'GET':
             serializer = UserEditSerializer(user)
             return Response(data=serializer.data, status=status.HTTP_200_OK)
@@ -40,11 +43,17 @@ class UserViewSet(viewsets.ModelViewSet):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(data=serializer.errors, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 @api_view(['POST'])
+@permission_classes([permissions.AllowAny])
 def signup(request):
+    if User.objects.filter(
+        username=request.POST.get('username', False),
+        email=request.POST.get('email', False)
+    ).exists():
+        return Response(request.data, status=status.HTTP_200_OK)
     serializer = SignupSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
