81a64d7e8f92795c48c78f85b3dba7cb770ca685
Денис Филиппов <denbfil@yandex.ru>
2023-02-06 21:26:32 +0300

    После внесения изменений по пересмотру всего проекта

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,14 @@
+from api.filters import TitleFilter
 from api.permissions import IsAdminOrReadOnly, IsAuthor
+from api.serializers import (CategorySerializer, CommentSerializer,
+                             GenreSerializer, ReviewSerializer,
+                             TitleEditSerializer, TitleGETSerializer)
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
 from reviews.models import Category, Genre, Review, Title
-
-from .filters import TitleFilter
-from .serializers import (CategorySerializer, CommentSerializer,
-                          GenreSerializer, ReviewSerializer,
-                          TitleEditSerializer, TitleGETSerializer)
 
 
 class CreateDestroyListViewSet(mixins.CreateModelMixin,
