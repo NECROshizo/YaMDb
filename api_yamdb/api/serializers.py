3880f17ce044f0229e190c9bcefe8d86131a5016
Petr Oganin <necroshizo@gmail.com>
2023-02-08 21:31:28 +0300

    fixit serializers review validate

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -80,7 +80,7 @@ class ReviewSerializer(serializers.ModelSerializer):
         """Проверка, что отзыв единственный на произведение"""
         request = self.context['request']
         author = request.user
-        title_id = self.context.get('view').kwargs.get('title_id')
+        title_id = self.context['view'].kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
 
         method_is_post = request.method == 'POST'
