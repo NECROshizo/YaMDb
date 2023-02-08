dc92a1b079cf17b42c880a75fa62da6c44fdcf52
NECROshizo <105547260+NECROshizo@users.noreply.github.com>
2023-02-08 21:49:12 +0300

    Update filters.py
    Flake8 fix

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -5,7 +5,10 @@ from reviews.models import Title
 
 class TitleFilter(filters.FilterSet):
     """Фильтр для модели произведений"""
-    category = filters.CharFilter(field_name='category__slug', lookup_expr='iexact')
+    category = filters.CharFilter(
+        field_name='category__slug',
+        lookup_expr='iexact'
+    )
     genre = filters.CharFilter(field_name='genre__slug', lookup_expr='iexact')
     name = filters.CharFilter(field_name='name', lookup_expr='contains')
 
